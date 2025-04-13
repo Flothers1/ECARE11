6d3d66bd34583b93b5e022e0beb84de441a76f8e
Flothers1 <ifreeze@flothers.com>
2025-04-13 16:44:54 +0200

    MA:Restyling  the Downlod files

---
+++ Constants/AuthorizationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ECARE.Constants
+{
+    public static class AuthorizationConstants
+    {
+        public const string LabAdmin = "LabAdmin";
+        public const string LabUser = "LabUser";
+        public const string Admin = "Admin";
+    }
+}
